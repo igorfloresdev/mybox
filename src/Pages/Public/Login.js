d471ddff448984e13578a4e3e45aceba5e918b25
igorfloresdev <dev.igorflores@gmail.com>
2023-03-21 16:40:11 -0300

    Created auth verification and added new toast

--- src/Pages/Public/Login.js
+++ src/Pages/Public/Login.js
@@ -4,24 +4,30 @@ import { useCookies } from 'react-cookie'
 import { BsBoxSeam } from 'react-icons/bs'
 import Card from '../../components/Card'
 import Input from '../../components/Input'
-import Toast from '../../components/Toast'
 import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-hot-toast'
 
 const Login = () => {
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   
-  const [toastText, setToastText] = useState('')
-  const [toastDisplay, setToastDisplay] = useState('')
 
-  const [cookies, setCookie, removeCookie] = useCookies(['authToken'])
+  const [cookies, setCookie, removeCookie] = useCookies(['authToken', 'userId', 'username'])
 
   let navigate = useNavigate()
 
   useEffect( () => {
     if(cookies.authToken) {
-      navigate('/auth/dashboard')
+      axios.get(`http://localhost:3001/users?authToken=${cookies.authToken}`)
+      .then(response => {
+        if(response.data[0]){
+          navigate('/auth')
+        } else {
+          removeCookie(['authToken', 'userId', 'username'])
+          navigate('/')
+        }
+      })
     }
   })
   
@@ -30,17 +36,17 @@ const Login = () => {
     .then(response => {
       if (response.data[0]) {
         setCookie('authToken', response.data[0].authToken)
+        setCookie('userId', response.data[0].id)
+        setCookie('username', response.data[0].username)
         navigate('/auth/dashboard')
       } else {
-        setToastText('Login ou senha incorreto !')
-        setToastDisplay(true)
+        toast.error('Login ou senha incorreto !')
       }
     })
   }
 
   return (
     <div className="flex gap-x-32 h-screen flex-row items-center justify-center">
-      <Toast text={toastText} display={toastDisplay} className="alert-error" duration={3000} />
       <BsBoxSeam size={400} />
       <Card title="My Box" text="Digite seu login e senha para acessar !" action={LoginUser} button="Entrar">
         <Input getValue={ value => setUsername(value) } type="text" placeholder="Usuário" className="mt-12" />
