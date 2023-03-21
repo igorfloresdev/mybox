b8b07e3b1f04de2a09516f69dc2d691671deed07
igorfloresdev <dev.igorflores@gmail.com>
2023-03-21 16:34:51 -0300

    Refactored Input Component

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,11 +1,25 @@
-const Input = ({type = "text", placeholder, className, getValue}) => {
+const Input = ({ type = "text", placeholder, className, getValue, label, size, required = false, value }) => {
 
     const inputValue = (event) => {
         getValue(event.target.value)
     }
 
     return (
-        <input onChange={inputValue} type={type} placeholder={placeholder} className={`input input-bordered w-full max-w-xs ${className}`} />
+        <div>
+            {label &&
+                <label className="label">
+                    <span className="label-text">{label}</span>
+                </label>
+            }
+            <input required={required}
+                onChange={inputValue}
+                size={size}
+                type={type}
+                placeholder={placeholder}
+                className={`input input-bordered w-full max-w-xs ${className}`}
+                value={value}
+            />
+        </div>
     )
 }
 
