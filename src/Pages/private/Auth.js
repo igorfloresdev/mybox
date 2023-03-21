bc351985366a2bb7847b0de4bfefdb0c20b15bb8
igorfloresdev <dev.igorflores@gmail.com>
2023-03-21 16:39:13 -0300

    Created function to verify auth and added Id on menu items

--- src/Pages/private/Auth.js
+++ src/Pages/private/Auth.js
@@ -1,19 +1,44 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useCookies } from 'react-cookie'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Navbar from '../../components/Navbar'
 
-const auth = () => {
+const Auth = () => {
+
+  const [cookies, setCookie, removeCookie] = useCookies(['authToken'])
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (cookies) {
+      axios.get(`http://localhost:3001/users?authToken=${cookies.authToken}`)
+      .then(response => {
+        if (!response.data[0]){
+          navigate('/')
+          console.log('unauthorized access')
+        }
+      })
+      .catch(response => {
+        navigate('/')
+        console.log(response)
+      })
+    }
+  })
 
   const menuItems = [
     {
+      id: 1,
       name: 'Dashboard',
       link: '/auth/dashboard'
     },
     {
+      id: 2,
       name: 'Produtos',
       link: '/auth/products'
     },
     {
+      id: 3,
       name: 'Categorias',
       link: '/auth/categories'
     }
@@ -28,4 +53,4 @@ const auth = () => {
   )
 }
 
-export default auth
+export default Auth
